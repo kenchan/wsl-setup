9b708804cb68dadcb6fb5c5731de6675f6771e2e
Kenichi Takahashi <kenichi.taka@gmail.com>
2020-04-12 17:16:51 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -u
+
+mkdir -p ~/src/github.com/kenchan
+git clone https://github.com/kenchan/wsl-ubuntu-setup.git ~/src/github.com/kenchan/wsl-ubuntu-setup
+
+pushd ~/src/github.com/kenchan/wsl-ubuntu-setup
+
+bin/setup
+bin/mitamae local recipe.rb
+
+popd
