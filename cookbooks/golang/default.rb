6069b2544db64aff8bca7adb11af9eee20e9dd6d
Kenichi Takahashi <kenichi.taka@gmail.com>
2020-04-22 21:14:33 +0900

    fix typo

--- cookbooks/golang/default.rb
+++ cookbooks/golang/default.rb
@@ -1,4 +1,4 @@
-execute 'add-apt-respository ppa:longsleep/golang-backports' do
+execute 'add-apt-repository ppa:longsleep/golang-backports' do
   user 'root'
   not_if 'apt-cache policy | grep -q ppa:longsleep/golang-backports'
 end
