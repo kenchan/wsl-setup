efcc0944e2739359d4f1a89b321e052f3891a6d6
Kenichi Takahashi <kenichi.taka@gmail.com>
2022-02-10 01:16:58 +0900

    install fish and fish plugins on Gentoo

--- cookbooks/fish/default.rb
+++ cookbooks/fish/default.rb
@@ -5,9 +5,9 @@ end
 execute 'install fisher' do
   command "curl https://git.io/fisher --create-dirs -sLo #{ENV['HOME']}/.config/fish/functions/fisher.fish"
   not_if "test -e #{ENV['HOME']}/.config/fish/functions/fisher.fish"
-  notifies :run, 'execute[fish -c "fisher"]'
+  notifies :run, 'execute[fish -c "fisher update"]'
 end
 
-execute 'fish -c "fisher"' do
+execute 'fish -c "fisher update"' do
   action :nothing
-end 
+end
--- platform/gentoo/default.rb
+++ platform/gentoo/default.rb
@@ -1,3 +1,4 @@
 include_recipe '../../cookbooks/eselect_repository/default.rb'
 include_recipe '../../cookbooks/portage/default.rb'
 include_recipe '../../cookbooks/dotfiles/default.rb'
+include_recipe '../../cookbooks/fish/default.rb'
