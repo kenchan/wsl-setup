2a8cebaf7221a3d47b5af835898d985e2a244433
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-03-03 02:18:41 +0900

    bump git to 2.39.2.windows.1

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -2,6 +2,6 @@ file "#{ENV['HOME']}/.config/git/config.local" do
   action :create
   content <<-EOS
 [credential]
-    helper = /mnt/c/Program\\\\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe
+    helper = /mnt/c/Program\\\\ Files/Git/mingw64/bin/git-credential-manager.exe
 EOS
 end
