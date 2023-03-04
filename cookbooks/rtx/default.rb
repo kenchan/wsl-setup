974685e7f672c7805c04a23550d88347ad19465d
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-03-04 18:13:48 +0900

    update

--- cookbooks/rtx/default.rb
+++ cookbooks/rtx/default.rb
@@ -1,10 +1,12 @@
-
 execute 'install rtx' do
   command <<-CMD
     curl https://rtx.jdxcode.com/rtx-latest-linux-x64 > #{ENV['HOME']}/.local/bin/rtx
     chmod +x #{ENV['HOME']}/.local/bin/rtx
 CMD
-  not_if 'command -v rtx'
+  not_if "test -e #{ENV['HOME']}/.local/bin/rtx"
 end
 
-execute 'rtx install -a'
+execute 'rtx install -a' do
+  action :nothing
+  subscribes :run, 'execute[install rtx]'
+end
