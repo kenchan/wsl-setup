f643567e29aa57d57b2fc1610b61f2e2453cc321
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-03-13 01:29:41 +0900

    arch

---
+++ platform/arch/default.rb
@@ -0,0 +1,65 @@
+# base-develはインストールがうまくいかない
+%w(
+  base-devel
+  git
+  neovim
+  wget
+  fish
+  unzip
+).each do |pkg|
+  package pkg
+end
+
+%w(
+  docker
+  docker-compose
+  kubectl
+  kubectx
+).each do |pkg|
+  package pkg
+end
+
+service 'docker' do
+  action [:enable, :start]
+end
+
+execute "gpasswd -a #{ENV['SUDO_USER']} docker" do
+  not_if "groups #{ENV['SUDO_USER']} | grep -q docker"
+end
+
+%w(
+  jq
+  tmux
+  fish
+  starship
+  git-delta
+  github-cli
+  sshuttle
+  bat
+  fd
+  lsd
+  ripgrep
+  sd
+  peco
+).each do |pkg|
+  package pkg
+end
+
+%w(
+  rust
+  ruby
+).each do |pkg|
+  package pkg
+end
+
+git 'https://aur.archlinux.org/paru.git' do
+  user ENV['SUDO_USER']
+  destination '/tmp/paru'
+  repository 'https://aur.archlinux.org/paru.git'
+end
+
+execute 'makepkg -si --noconfirm' do
+  user ENV['SUDO_USER']
+  cwd '/tmp/paru'
+  not_if 'command -v paru > /dev/null'
+end
