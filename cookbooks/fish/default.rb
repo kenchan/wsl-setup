da40e25e6762e649325e651b89cef8f66824c246
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-03-17 00:33:44 +0900

    Consider cases when fisher is installed via pacman

--- cookbooks/fish/default.rb
+++ cookbooks/fish/default.rb
@@ -4,7 +4,7 @@ end
 
 execute 'install fisher' do
   command "curl https://git.io/fisher --create-dirs -sLo #{ENV['HOME']}/.config/fish/functions/fisher.fish"
-  not_if "test -e #{ENV['HOME']}/.config/fish/functions/fisher.fish"
+  not_if "test -e #{ENV['HOME']}/.config/fish/functions/fisher.fish || test -e /usr/share/fish/vendor_functions.d/fisher.fish"
   notifies :run, 'execute[fish -c "fisher update"]'
 end
 
