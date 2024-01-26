73d51a4d25a88d2be990bb93decf269081cc7b97
Kenichi Takahashi <kenichi.taka@gmail.com>
2024-01-27 00:56:47 +0900

    use paru

--- platform/arch/default.rb
+++ platform/arch/default.rb
@@ -1,3 +1,46 @@
+git 'https://aur.archlinux.org/paru.git' do
+  user ENV['SUDO_USER']
+  destination '/tmp/paru'
+  repository 'https://aur.archlinux.org/paru.git'
+  notifies :run, 'execute[makepkg -si --noconfirm]', :immediately
+  not_if 'command -v paru > /dev/null'
+end
+
+execute 'makepkg -si --noconfirm' do
+  action :nothing
+  user ENV['SUDO_USER']
+  cwd '/tmp/paru'
+end
+
+module Paru
+  def check_is_installed(package,version=nil)
+    if version
+      grep = version.include?('-') ? "^#{escape(version)}$" : "^#{escape(version)}-"
+      "paru -Q #{escape(package)} | awk '{print $2}' | grep '#{grep}'"
+    else
+      "paru -Q #{escape(package)} || paru -Qg #{escape(package)}"
+    end
+  end
+
+  def get_version(package, opts=nil)
+    "paru -Qi #{package} | grep Version | awk '{print $3}'"
+  end
+
+  def install(package, version=nil, option='')
+    "paru -S --noconfirm #{option} #{package}"
+  end
+
+  def sync_repos
+    "paru -Syy"
+  end
+
+  def remove(package, option='')
+    "pacman -R --noconfirm #{option} #{package}"
+  end
+end
+
+Specinfra::Command::Arch::Base::Package.extend Paru
+
 include_recipe '../../cookbooks/locale/default.rb'
 
 # base-develはインストールがうまくいかない
@@ -62,27 +105,7 @@ end
   package pkg
 end
 
-git 'https://aur.archlinux.org/paru.git' do
-  user ENV['SUDO_USER']
-  destination '/tmp/paru'
-  repository 'https://aur.archlinux.org/paru.git'
-  notifies :run, 'execute[makepkg -si --noconfirm]', :immediately
-  not_if 'command -v paru > /dev/null'
-end
-
-execute 'makepkg -si --noconfirm' do
-  action :nothing
-  user ENV['SUDO_USER']
-  cwd '/tmp/paru'
-end
-
-define :aur_package do
-  execute "paru -S -a --noconfirm #{params[:name]}" do
-    user ENV['SUDO_USER']
-    not_if "pacman -Qm | grep -q #{params[:name]}"
-  end
-end
-
+# 1password
 execute 'gpg --receive-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22' do
   user ENV['SUDO_USER']
   not_if 'gpg -k | grep -q 3FEF9748469ADBE15DA7CA80AC2D62742012EA22'
@@ -98,7 +121,7 @@ end
   kagiana
   frgm
 ).each do |pkg|
-  aur_package pkg
+  package pkg
 end
 
 file '/etc/pacman.conf' do
