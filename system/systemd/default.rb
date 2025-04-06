165670bda30250659e36e875a465c2dfd2d27307
Kenichi Takahashi <kenichi.taka@gmail.com>
2025-04-06 21:43:22 +0900

    update

--- system/systemd/default.rb
+++ system/systemd/default.rb
@@ -11,3 +11,18 @@ remote_file '/etc/systemd/resolved.conf.d/dns_servers.conf' do
   mode '644'
   source 'files/etc/systemd/resolved.conf.d/dns_servers.conf'
 end
+
+execute 'enable and start systemd-resolved' do
+  command 'systemctl enable systemd-resolved && systemctl start systemd-resolved'
+  not_if 'systemctl is-active systemd-resolved'
+end
+
+execute 'mask systemd-firstboot' do
+  command 'systemctl mask systemd-firstboot'
+  not_if 'systemctl is-enabled systemd-firstboot | grep masked'
+end
+
+execute 'disable and stop systemd-networkd-wait-online' do
+  command 'systemctl disable systemd-networkd-wait-online && systemctl stop systemd-networkd-wait-online'
+  only_if 'systemctl is-enabled systemd-networkd-wait-online'
+end
