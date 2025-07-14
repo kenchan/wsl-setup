fe500365b6f0f2cb47cbbb54fa9e37b1c163e065
Kenichi Takahashi <kenichi.taka@gmail.com>
2025-07-14 22:58:55 +0900

    hi

--- system/sudoers/default.rb
+++ system/sudoers/default.rb
@@ -4,15 +4,15 @@ directory '/etc/sudoers.d' do
   group 'root'
 end
 
-remote_file '/etc/sudoers.d/01-gentoo-emerge' do
-  source 'files/etc/sudoers.d/01-gentoo-emerge'
+remote_file '/etc/sudoers.d/10-gentoo-emerge' do
+  source 'files/etc/sudoers.d/10-gentoo-emerge'
   mode '0440'
   owner 'root'
   group 'root'
 end
 
-remote_file '/etc/sudoers.d/10-wheel-group' do
-  source 'files/etc/sudoers.d/10-wheel-group'
+remote_file '/etc/sudoers.d/00-wheel-group' do
+  source 'files/etc/sudoers.d/00-wheel-group'
   mode '0440'
   owner 'root'
   group 'root'
