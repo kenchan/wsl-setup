6bd4c5b3d1c24f2e4e38b16cfd09be737675e266
Kenichi Takahashi <kenichi.taka@gmail.com>
2025-12-02 20:37:54 +0900

    Install wsl2-ssh-agent using mise

--- user/ssh/default.rb
+++
@@ -1,9 +0,0 @@
-directory "#{ENV['HOME']}/.local/bin"
-
-execute 'download and setup wsl2-ssh-agent' do
-  command <<-EOS
-    curl -L -o #{ENV['HOME']}/.local/bin/wsl2-ssh-agent https://github.com/mame/wsl2-ssh-agent/releases/latest/download/wsl2-ssh-agent
-    chmod +x #{ENV['HOME']}/.local/bin/wsl2-ssh-agent
-  EOS
-  not_if "test -f #{ENV['HOME']}/.local/bin/wsl2-ssh-agent"
-end
